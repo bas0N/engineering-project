4df2480fd37d6af34677f37b38e5ec0b29b7935a
SKupisz <szym-ku@wp.pl>
2024-11-02 16:23:03 +0100

    Added Navbar in the container app with the link leading to basket

---
+++ Frontend/Main/src/components/navbar/Navbar.styled.tsx
@@ -0,0 +1,24 @@
+import { Badge, Button } from '@fluentui/react-components';
+import styled from "styled-components";
+
+export const NavbarContainer = styled.nav`
+    width: calc(100% - 10px);
+    padding: 0px 5px;
+    display: flex;
+    flex-direction: row;
+    gap: 8px;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const BasketButton = styled(Button).attrs({appearance: 'transparent'})`
+    padding: 10px 20px;
+    border-radius: 10px;
+    color: white;
+    text-decoration: none;
+`;
+
+export const BasketButtonBadge = styled(Badge).attrs({appearance: 'ghost', size: 'extra-large'})`
+    font-size: 16px;
+    color: inherit;
+`;
