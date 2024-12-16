dde38428edd13a1c7b8dc8f9876aa1bba8c0f4cf
basON <wch.basinski@gmail.com>
2024-12-16 21:43:51 +0100

    update recc sytem 1 and api gateway

--- Backend/PythonBackend/app/scripts/load_data.py
+++
@@ -1,4 +0,0 @@
-## 1. take url, download file and save it with a given name
-## 2. read it in batches, remove unused columns, clean data, overwrite normalisation config, and save to new file
-## 3. read it in batches, perform normalisation
-## 3. from the new file, create the price normaliser
