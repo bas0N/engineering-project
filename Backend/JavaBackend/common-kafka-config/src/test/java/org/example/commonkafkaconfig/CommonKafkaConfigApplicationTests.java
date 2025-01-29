8d558732d34529a24811bf89e12256e71cdb4aeb
OganKuba <kubog123@gmail.com>
2025-01-29 19:40:46 +0100

    pom.xml

---
+++ Backend/JavaBackend/common-kafka-config/src/test/java/org/example/commonkafkaconfig/CommonKafkaConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.commonkafkaconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonKafkaConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
